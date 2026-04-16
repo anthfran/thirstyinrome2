0b05d906271b40fba5b70805f81971d4028313e9
Anthony Francella <anthonyf@gmail.com>
2026-04-15 22:01:00 -0400

    feat: inject PlaceViewModel into SwiftUI environment at app root

--- thirstyinrome/thirstyinromeApp.swift
+++ thirstyinrome/thirstyinromeApp.swift
@@ -1,17 +1,13 @@
-//
-//  thirstyinromeApp.swift
-//  thirstyinrome
-//
-//  Created by Anthony Francella on 2026-04-15.
-//
-
 import SwiftUI
 
 @main
 struct thirstyinromeApp: App {
+    @State private var viewModel = PlaceViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(viewModel)
         }
     }
 }
