89d6376c0dd80fd0ea38669a157d8028c63b4522
Anthony Francella <anthonyf@gmail.com>
2026-04-15 20:07:35 -0400

    Initial Commit

---
+++ thirstyinrome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thirstyinrome
+//
+//  Created by Anthony Francella on 2026-04-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ thirstyinrome/thirstyinromeApp.swift
@@ -0,0 +1,17 @@
+//
+//  thirstyinromeApp.swift
+//  thirstyinrome
+//
+//  Created by Anthony Francella on 2026-04-15.
+//
+
+import SwiftUI
+
+@main
+struct thirstyinromeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
